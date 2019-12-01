27ddc6024a88b337e302572664066cc8481d5e22
Patience Deaton <46998183+PatienceD@users.noreply.github.com>
2019-12-01 16:57:01 -0500

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+// import Navbar from "./components/Navbar";
+// import Body from "./components/Body";
+import Home from "./components/Home";
+import Wisconsin from "./components/Wisconsin";
+// import Footer from "./components/Footer";
+import Schools from "./components/Schools";
+import NoSchools from "./components/NoSchools";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="wrapper">
+        <Router>
+          {/* <Navbar /> */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/wisconsin" component={Wisconsin} />
+            <Route exact path="/schools" component={Schools} />
+            <Route component={NoSchools} />
+          </Switch>
+        </Router>
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
